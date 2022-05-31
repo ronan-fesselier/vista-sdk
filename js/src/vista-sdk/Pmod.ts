30273fca2837ecaa0e33e84abf34b094df03d60a
Saastad <Kristoffer.Saastad@dnv.com>
2022-05-31 09:52:22 +0200

    npm init and build

--- js/src/vista-sdk/Pmod.ts
+++ js/src/vista-sdk/Pmod.ts
@@ -1,59 +1,61 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { Gmod, GmodDto, GmodNodeDto, ImoNumber } from '.';
-import { GmodRelationDto } from './types/GmodDto';
-import { PmodInfo } from './types/Pmod';
-import { PmodDto } from './types/PmodDto';
+import { Gmod, GmodDto, GmodNodeDto, ImoNumber } from ".";
+import { GmodRelationDto } from "./types/GmodDto";
+import { PmodInfo } from "./types/Pmod";
+import { PmodDto } from "./types/PmodDto";
 
 export class Pmod {
-  private _model: Gmod;
-  private _info?: PmodInfo;
+    private _model: Gmod;
+    private _info?: PmodInfo;
 
-  private constructor(model: Gmod, info?: PmodInfo) {
-    this._model = model;
-    this._info = info;
-  }
+    private constructor(model: Gmod, info?: PmodInfo) {
+        this._model = model;
+        this._info = info;
+    }
 
-  public static createFromDto(pmodDto: PmodDto, gmod: Gmod) {
-    const gmodDto: GmodDto = {
-      items: pmodDto.items.map(item => {
-        const node = gmod.getNode(item.code);
-        return {
-          category: node.metadata.category,
-          code: item.code,
-          name: item.name,
-          type: node.metadata.type,
-          commonDefinition: node.metadata.commonDefinition,
-          commonName: item.name,
-          definition: node.metadata.definition,
-          installSubstructure: node.metadata.installSubstructure,
-          normalAssignmentNames: Object.fromEntries(node.metadata.normalAssignmentNames),
-          location: item.location,
-          id: item.id,
-        } as GmodNodeDto;
-      }),
-      relations: pmodDto.relations.map<GmodRelationDto>(r => ({
-        parent: r[0],
-        child: r[1],
-      })),
-      visVersion: gmod.visVersion,
-    };
-    return new Pmod(new Gmod(gmod.visVersion, gmodDto), {
-      imoNumber: new ImoNumber(pmodDto.info.imo_number),
-      timestamp: new Date(pmodDto.info.timestamp),
-      vesselId: pmodDto.info.vessel_id,
-      vesselName: pmodDto.info.vessel_name,
-    });
-  }
+    public static createFromDto(pmodDto: PmodDto, gmod: Gmod) {
+        const gmodDto: GmodDto = {
+            items: pmodDto.items.map((item) => {
+                const node = gmod.getNode(item.code);
+                return {
+                    category: node.metadata.category,
+                    code: item.code,
+                    name: item.name,
+                    type: node.metadata.type,
+                    commonDefinition: node.metadata.commonDefinition,
+                    commonName: item.name,
+                    definition: node.metadata.definition,
+                    installSubstructure: node.metadata.installSubstructure,
+                    normalAssignmentNames: Object.fromEntries(
+                        node.metadata.normalAssignmentNames
+                    ),
+                    location: item.location,
+                    id: item.id,
+                } as GmodNodeDto;
+            }),
+            relations: pmodDto.relations.map<GmodRelationDto>((r) => ({
+                parent: r[0],
+                child: r[1],
+            })),
+            visRelease: gmod.visVersion,
+        };
+        return new Pmod(new Gmod(gmod.visVersion, gmodDto), {
+            imoNumber: new ImoNumber(pmodDto.info.imo_number),
+            timestamp: new Date(pmodDto.info.timestamp),
+            vesselId: pmodDto.info.vessel_id,
+            vesselName: pmodDto.info.vessel_name,
+        });
+    }
 
-  public static create(gmod: Gmod, info?: PmodInfo) {
-    return new Pmod(gmod, info);
-  }
+    public static create(gmod: Gmod, info?: PmodInfo) {
+        return new Pmod(gmod, info);
+    }
 
-  public get model() {
-    return this._model;
-  }
+    public get model() {
+        return this._model;
+    }
 
-  public get info() {
-    return this._info;
-  }
+    public get info() {
+        return this._info;
+    }
 }
--- js/src/vista-sdk/types/GmodDto.ts
+++ js/src/vista-sdk/types/GmodDto.ts
@@ -1,24 +1,24 @@
 export interface GmodDto {
-  visVersion: string;
-  items: GmodNodeDto[];
-  relations: GmodRelationDto[];
+    visRelease: string;
+    items: GmodNodeDto[];
+    relations: GmodRelationDto[];
 }
 
 export interface GmodRelationDto {
-  parent: string;
-  child: string;
+    parent: string;
+    child: string;
 }
 
 export interface GmodNodeDto {
-  id?: string;
-  category: string;
-  type: string;
-  code: string;
-  name: string;
-  commonName?: string | null;
-  definition?: string | null;
-  commonDefinition?: string | null;
-  installSubstructure?: boolean | null;
-  normalAssignmentNames?: { [key: string]: string };
-  location?: string;
+    id?: string;
+    category: string;
+    type: string;
+    code: string;
+    name: string;
+    commonName?: string | null;
+    definition?: string | null;
+    commonDefinition?: string | null;
+    installSubstructure?: boolean | null;
+    normalAssignmentNames?: { [key: string]: string };
+    location?: string;
 }
