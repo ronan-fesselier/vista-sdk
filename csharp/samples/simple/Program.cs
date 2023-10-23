9aeb34c653e93edf42fc2f09de9ee463519fede9
Martin Othamar <Martin.Othamar@dnv.com>
2023-10-23 10:34:06 +0200

    Sample code for generating timeseriesdatapackage in C# 2

---
+++ csharp/samples/simple/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using Vista.SDK.Transport.Json;
+
+var sourceData = new[]
+{
+    new BallastEvent(
+        Mode: "Stripping",
+        Location: "IMO",
+        Start: DateTimeOffset.Parse("2023-09-13T21:04:38.00Z", CultureInfo.InvariantCulture),
+        Stop: DateTimeOffset.Parse("2023-09-13T23:56:00.00Z", CultureInfo.InvariantCulture),
+        Volume: 740,
+        UVTreatment: 92,
+        Salinity: 0,
+        Position: (62.84933, 7.120334)
+    )
+};
+var package = TimeSeries.GetPackage(sourceData);
+var json = package.Serialize();
+
+Console.WriteLine($"Produced TimeSeriesDataPackage:\n{json}");
---
+++ csharp/samples/simple/TimeSeriesDataPackage.cs
@@ -0,0 +1,85 @@
+using System.Globalization;
+using Vista.SDK.Transport.Json.TimeSeriesData;
+
+record BallastEvent(
+    string Mode,
+    string Location,
+    DateTimeOffset Start,
+    DateTimeOffset Stop,
+    int Volume,
+    int UVTreatment,
+    int Salinity,
+    (double Lat, double Lng) Position
+);
+
+static class TimeSeries
+{
+    internal static TimeSeriesDataPackage GetPackage(IReadOnlyList<BallastEvent> sourceData)
+    {
+        // Shipboard server extracts sensor data and prepares to ship it to shore using the TimeSeriesDataPackage
+        var now = DateTimeOffset.UtcNow;
+
+        var dataChannelId = new[]
+        {
+            "Mode",
+            "Location",
+            "Start",
+            "Stop",
+            "Volume",
+            "UVTreatment",
+            "Salinity",
+            "Latitude",
+            "Longitude",
+        };
+        var dataAsTable = sourceData
+            .Select(
+                e =>
+                    (
+                        Timestamp: e.Start,
+                        Data: new[]
+                        {
+                            e.Mode,
+                            e.Location,
+                            e.Start.ToString(),
+                            e.Stop.ToString(),
+                            e.Volume.ToString(CultureInfo.InvariantCulture),
+                            e.UVTreatment.ToString(CultureInfo.InvariantCulture),
+                            e.Salinity.ToString(CultureInfo.InvariantCulture),
+                            e.Position.Lat.ToString(CultureInfo.InvariantCulture),
+                            e.Position.Lng.ToString(CultureInfo.InvariantCulture),
+                        }
+                    )
+            )
+            .ToArray();
+
+        var from = sourceData.Min(d => d.Start);
+        var to = sourceData.Max(d => d.Stop);
+
+        var data = new TimeSeriesData(
+            dataConfiguration: null, // May optionally refer to a specific DataChannelList,
+            eventData: new EventData(Array.Empty<EventDataSet>(), "0"),
+            tabularData: new[]
+            {
+                new TabularData(
+                    dataChannelID: dataChannelId,
+                    dataSet: dataAsTable
+                        .Select(d => new TabularDataSet(quality: null, timeStamp: d.Timestamp, value: d.Data))
+                        .ToArray(),
+                    numberOfDataChannel: dataChannelId.Length.ToString(CultureInfo.InvariantCulture),
+                    numberOfDataSet: dataChannelId.Length.ToString(CultureInfo.InvariantCulture) // One sample per datachannel/timestamp for this scenario
+                )
+            }
+        );
+
+        var header = new Header(
+            author: "DNV",
+            dateCreated: now,
+            dateModified: now,
+            shipID: "1234567",
+            systemConfiguration: Array.Empty<ConfigurationReference>(), // A reference to the source system, e.g. DBMS
+            timeSpan: new Vista.SDK.Transport.Json.TimeSeriesData.TimeSpan(from, to) // Oldest and newest dataset
+        );
+        var package = new TimeSeriesDataPackage(new Package(header, new[] { data }));
+        return package;
+    }
+}
