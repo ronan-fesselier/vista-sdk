9b3f7a399181ea349b8bfb035b3c801a63707ba6
Ronan F. <ronan.fesselier@etik.com>
2026-07-07 05:48:28 +0200

    feat(cpp): add VIS singleton

---
+++ cpp/cmake/dnvVistaSdkSources.cmake
@@ -0,0 +1,9 @@
+#==============================================================================
+# dnv-vista-sdk - CMake Sources
+#==============================================================================
+
+set(dnv_vista_sdk_sources)
+
+list(APPEND dnv_vista_sdk_sources
+    ${DNV_VISTA_SDK_SOURCE_DIR}/SDK/core/VIS.cpp
+)
