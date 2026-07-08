4aceb6e1cad047aabf9e0bab52dbb70f5aa72985
Ronan F. <ronan.fesselier@etik.com>
2026-07-08 07:59:02 +0200

    feat(cpp): add EmbeddedResources loader (decompress and deserialize embedded JSON resources)

--- cpp/cmake/dnvVistaSdkSources.cmake
+++ cpp/cmake/dnvVistaSdkSources.cmake
@@ -8,6 +8,8 @@ list(APPEND dnv_vista_sdk_sources
     ${DNV_VISTA_SDK_SOURCE_DIR}/Compression/Gzip.cpp
     ${DNV_VISTA_SDK_SOURCE_DIR}/Compression/Inflate.cpp
 
+    ${DNV_VISTA_SDK_SOURCE_DIR}/EmbeddedResources/EmbeddedResources.cpp
+
     ${DNV_VISTA_SDK_SOURCE_DIR}/JSON/Builder.cpp
     ${DNV_VISTA_SDK_SOURCE_DIR}/JSON/Document.cpp
 
