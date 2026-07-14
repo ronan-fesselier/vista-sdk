fb63b72358f2cbf24080c3ee65cc3d738cc96d30
Ronan F. <ronan.fesselier@etik.com>
2026-07-14 13:58:16 +0200

    feat(cpp): add ISO 19848 source generator

---
+++ cpp/cmake/dnvVistaSdkISO19848VersionsGenerator.cmake
@@ -0,0 +1,34 @@
+#==============================================================================
+# dnv::vista::sdk::ISO19848VersionsGenerator
+#==============================================================================
+
+add_executable(dnv-vista-sdk-iso19848versionsgenerator
+    ${DNV_VISTA_SDK_SOURCE_DIR}/SourceGenerators/ISO19848VersionsGenerator.cpp
+)
+add_executable(dnv::vista::sdk::iso19848versionsgenerator ALIAS dnv-vista-sdk-iso19848versionsgenerator)
+
+set_target_properties(dnv-vista-sdk-iso19848versionsgenerator
+    PROPERTIES
+        CXX_STANDARD          20
+        CXX_STANDARD_REQUIRED ON
+        CXX_EXTENSIONS        OFF
+        DEBUG_POSTFIX         "-d"
+)
+
+set(DNV_VISTA_SDK_ISO19848_GENERATED_HEADER     "${DNV_VISTA_SDK_INCLUDE_DIR}/dnv/vista/sdk/transport/ISO19848Versions.h")
+set(DNV_VISTA_SDK_ISO19848_GENERATED_EXTENSIONS "${DNV_VISTA_SDK_SOURCE_DIR}/SDK/transport/ISO19848VersionsExtensions.h")
+
+add_custom_command(
+    OUTPUT "${DNV_VISTA_SDK_ISO19848_GENERATED_HEADER}" "${DNV_VISTA_SDK_ISO19848_GENERATED_EXTENSIONS}"
+    COMMAND $<TARGET_FILE:dnv-vista-sdk-iso19848versionsgenerator> "${DNV_VISTA_SDK_ISO19848_GENERATED_HEADER}" "${DNV_VISTA_SDK_ISO19848_GENERATED_EXTENSIONS}" "${DNV_VISTA_SDK_VERSION}"
+    COMMAND clang-format -i "${DNV_VISTA_SDK_ISO19848_GENERATED_HEADER}" || ${CMAKE_COMMAND} -E true
+    COMMAND clang-format -i "${DNV_VISTA_SDK_ISO19848_GENERATED_EXTENSIONS}" || ${CMAKE_COMMAND} -E true
+    DEPENDS dnv-vista-sdk-iso19848versionsgenerator
+    WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+    COMMENT "Generating ISO19848Versions.h and ISO19848VersionsExtensions.h from embedded resources..."
+    VERBATIM
+)
+
+add_custom_target(dnv-vista-sdk-generate-iso19848versions ALL
+    DEPENDS "${DNV_VISTA_SDK_ISO19848_GENERATED_HEADER}" "${DNV_VISTA_SDK_ISO19848_GENERATED_EXTENSIONS}"
+)
