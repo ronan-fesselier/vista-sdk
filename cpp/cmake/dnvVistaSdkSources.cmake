a03dbff797f7a559d2d64cdb013f93190791dc95
Ronan F. <ronan.fesselier@etik.com>
2026-07-15 21:53:34 +0200

    feat(cpp): add DataChannel domain model for ISO 19848 DataChannelList messages

--- cpp/cmake/dnvVistaSdkSources.cmake
+++ cpp/cmake/dnvVistaSdkSources.cmake
@@ -36,6 +36,7 @@ list(APPEND dnv_vista_sdk_sources
     ${DNV_VISTA_SDK_SOURCE_DIR}/SDK/query/MetadataTagsQuery.cpp
 
     ${DNV_VISTA_SDK_SOURCE_DIR}/SDK/transport/serialization/json/SerializableDocument.cpp
+    ${DNV_VISTA_SDK_SOURCE_DIR}/SDK/transport/datachannel/DataChannel.cpp
     ${DNV_VISTA_SDK_SOURCE_DIR}/SDK/transport/ISO19848.cpp
     ${DNV_VISTA_SDK_SOURCE_DIR}/SDK/transport/ShipId.cpp
 
