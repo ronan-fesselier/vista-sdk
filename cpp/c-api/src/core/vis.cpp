5dc0b9414601857f8bd95d7923ea0b6d93cc2450
Ronan F․ <ronan.fesselier@etik.com>
2026-07-28 23:05:11 +0200

    feat(cpp): add VIS C API wrapper

---
+++ cpp/c-api/src/core/vis.cpp
@@ -0,0 +1,52 @@
+#include "dnv/vista/sdk/c/core/vis.h"
+
+#include "../cast_internal.h"
+#include "../error_internal.h"
+
+using namespace dnv::vista::sdk;
+using dnv::vista::sdk::c::toVis;
+
+const dnv_vista_sdk_vis_t* dnv_vista_sdk_vis_instance(void)
+{
+    return reinterpret_cast<const dnv_vista_sdk_vis_t*>(&VIS::instance());
+}
+
+size_t dnv_vista_sdk_vis_version_count(const dnv_vista_sdk_vis_t* vis)
+{
+    if (vis == nullptr)
+    {
+        c::setLastError("vis must not be null", DNV_VISTA_SDK_ERROR_INVALID_ARGUMENT);
+        return 0;
+    }
+
+    return toVis(vis)->versions().size();
+}
+
+const char* dnv_vista_sdk_vis_version_at(const dnv_vista_sdk_vis_t* vis, size_t index)
+{
+    if (vis == nullptr)
+    {
+        c::setLastError("vis must not be null", DNV_VISTA_SDK_ERROR_INVALID_ARGUMENT);
+        return nullptr;
+    }
+
+    const auto versions = toVis(vis)->versions();
+    if (index >= versions.size())
+    {
+        c::setLastError("index out of range", DNV_VISTA_SDK_ERROR_OUT_OF_RANGE);
+        return nullptr;
+    }
+
+    return VisVersions::toString(versions[index]).data();
+}
+
+const char* dnv_vista_sdk_vis_latest(const dnv_vista_sdk_vis_t* vis)
+{
+    if (vis == nullptr)
+    {
+        c::setLastError("vis must not be null", DNV_VISTA_SDK_ERROR_INVALID_ARGUMENT);
+        return nullptr;
+    }
+
+    return VisVersions::toString(toVis(vis)->latest()).data();
+}
