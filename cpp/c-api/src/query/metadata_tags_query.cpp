dae9d39cfa405e59f8fa181413440301f43e251f
Ronan F. <ronan.fesselier@etik.com>
2026-08-05 09:11:27 +0200

    feat(c-api): add MetadataTagsQuery/MetadataTagsQueryBuilder C API wrapper

---
+++ cpp/c-api/src/query/metadata_tags_query.cpp
@@ -0,0 +1,38 @@
+#include "dnv/vista/sdk/c/query/metadata_tags_query.h"
+
+#include "../cast_internal.h"
+#include "../error_internal.h"
+
+using namespace dnv::vista::sdk;
+using dnv::vista::sdk::c::fromMetadataTagsQueryBuilderRef;
+using dnv::vista::sdk::c::toLocalId;
+using dnv::vista::sdk::c::toMetadataTagsQuery;
+
+void dnv_vista_sdk_metadata_tags_query_free(dnv_vista_sdk_metadata_tags_query_t* query)
+{
+    delete reinterpret_cast<MetadataTagsQuery*>(query);
+}
+
+int dnv_vista_sdk_metadata_tags_query_match(
+    const dnv_vista_sdk_metadata_tags_query_t* query, const dnv_vista_sdk_local_id_t* localId)
+{
+    if (query == nullptr || localId == nullptr)
+    {
+        c::setLastError("query and localId must not be null", DNV_VISTA_SDK_ERROR_INVALID_ARGUMENT);
+        return 0;
+    }
+
+    return toMetadataTagsQuery(query)->match(*toLocalId(localId)) ? 1 : 0;
+}
+
+const dnv_vista_sdk_metadata_tags_query_builder_t* dnv_vista_sdk_metadata_tags_query_builder(
+    const dnv_vista_sdk_metadata_tags_query_t* query)
+{
+    if (query == nullptr)
+    {
+        c::setLastError("query must not be null", DNV_VISTA_SDK_ERROR_INVALID_ARGUMENT);
+        return nullptr;
+    }
+
+    return fromMetadataTagsQueryBuilderRef(toMetadataTagsQuery(query)->builder());
+}
