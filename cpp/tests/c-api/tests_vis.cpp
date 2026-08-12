054f4da925a174538819d028aa010affc66130f5
Ronan F. <ronan.fesselier@etik.com>
2026-08-12 21:14:03 +0200

    feat(c-api): add <dnv/VistaSDK_c_api.h> top-level umbrella header for the C API

--- cpp/tests/c-api/tests_vis.cpp
+++ cpp/tests/c-api/tests_vis.cpp
@@ -1,12 +1,6 @@
 #include <doctest/doctest.h>
 
-#include <dnv/vista/sdk/c/core/gmod.h>
-#include <dnv/vista/sdk/c/core/gmod_path.h>
-#include <dnv/vista/sdk/c/core/local_id.h>
-#include <dnv/vista/sdk/c/core/local_id_builder.h>
-#include <dnv/vista/sdk/c/core/vis.h>
-#include <dnv/vista/sdk/c/common.h>
-#include <dnv/vista/sdk/c/error.h>
+#include <dnv/VistaSDK_c_api.h>
 
 #include <string_view>
 
